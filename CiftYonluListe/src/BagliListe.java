f2bbbff2363fb6609e06d5ddc507c4fe32e72af2
aydog4nn <zephyrflaner@gmail.com>
2024-11-21 18:08:13 +0300

    insertion-in-linkedList

---
+++ CiftYonluListe/src/BagliListe.java
@@ -0,0 +1,91 @@
+public class BagliListe {
+    Node head = null;
+    Node tail = null;
+
+
+    void basaEkle(int data){
+        Node eleman = new Node(data);
+        if(head == null){
+            head = eleman;
+            tail = eleman;
+        }
+        else{
+            eleman.next = head;
+            head.prev = eleman;
+            head = eleman;
+        }
+    }
+
+    void arayaEkle(int indis,int data){
+        Node eleman = new Node(data);
+        if(head == null){
+            head = eleman;
+            tail = eleman;
+        }
+        else if (head != null && indis == 0 ){
+            eleman.next = head;
+            head.prev = eleman;
+            head = eleman;
+        }
+        else{
+            int n = 0;
+            Node temp = head;
+            while(temp != null){
+                temp = temp.next;
+                n++;
+            }
+            temp = head;
+            if (indis > n){
+                tail.next = eleman;
+                eleman.prev = tail;
+                tail = eleman;
+            }
+            else {
+                int i = 0;
+                while (i != indis){
+                    temp = temp.next;
+                    i++;
+                }
+                eleman.prev = temp.prev;
+                temp.prev.next = eleman;
+                eleman.next = temp;
+                temp.prev = eleman;
+            }
+        }
+    }
+
+
+
+
+    void sonaEkle(int data){
+        Node eleman = new Node(data);
+        if(head == null){
+            head = eleman;
+            tail = eleman;
+        }
+        else{
+            tail.next = eleman;
+            eleman.prev = tail;
+            tail = eleman;
+        }
+    }
+
+    void yazdir(){
+        Node temp = head;
+        System.out.print("Bas->");
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("Son");
+    }
+    void sondanYazdir(){
+        Node temp = tail;
+        System.out.print("Son->");
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.prev;
+        }
+        System.out.println("Bas");
+    }
+}
--- CiftYonluListe/src/Main.java
+++ CiftYonluListe/src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        BagliListe liste = new BagliListe();
+
+        int secim = -1,sayi,indis;
+        while (secim != 0){
+            System.out.println("1=Basa Ekle");
+            System.out.println("2=Sona Ekle");
+            System.out.println("3=Araya Ekle");
+            System.out.println("4=Sondan Yazdir.");
+            System.out.println("0-Cikis");
+            System.out.println("Seciminiz:");
+
+            secim = scanner.nextInt();
+            if (secim == 1){
+                System.out.println("Sayi:");
+                sayi = scanner.nextInt();
+                liste.basaEkle(sayi);
+            }
+            else if (secim == 2){
+                System.out.println("Sayi:");
+                sayi = scanner.nextInt();
+                liste.sonaEkle(sayi);
+            }
+            else if (secim == 3){
+                System.out.println("Indis");
+                indis = scanner.nextInt();
+                System.out.println("Sayi:");
+                sayi = scanner.nextInt();
+                liste.arayaEkle(indis,sayi);
+            }
+            liste.yazdir();
+        }
+
     }
 }

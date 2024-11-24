5c4016583f3b4e828e2729259cb7572553f41e7b
aydog4nn <zephyrflaner@gmail.com>
2024-11-25 02:25:15 +0300

    practiceQueue

---
+++ QueuePractice1/src/QueueYapisi.java
@@ -0,0 +1,16 @@
+public class QueueYapisi {
+    Node front;
+    Node rear;
+    int size;
+    int count;
+
+    public QueueYapisi(int size) {
+        this.size = size;
+        count = 0;
+        front = null;
+        rear = null;
+    }
+
+
+
+}
